0bf0c9d78fcdb4a95d22a25a9c03a73e5743d634
Tien Tran <tientran56151@gmail.com>
2020-02-20 20:43:03 -0600

    Updated Car methods with Dr. J

---
+++ Car.java
@@ -0,0 +1,41 @@
+
+public class Car {
+	private int range;
+
+	public Car(int data) {
+		this.range = data;		
+	}
+		
+	public boolean equals(Car a) {
+		if (a.getClass() == this.getClass())
+			System.out.println("At least it is the same class");
+
+		System.out.println("Running this special case");
+		return this.range == a.range;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Car) {
+			System.out.println("They are the same instance");
+			//I already know it is a Car, so I will act accordingly.
+			return this.equals((Car)o);
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		String str = "This vehicle has a range of " + Integer.toString(range)
+		+ " miles";
+		return str;
+	}
+	
+	public int milesToGo(int milesDriven) {
+		return range - milesDriven;
+		
+	}
+}
